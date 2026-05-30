d7bd11e9b403c4356db24721edf436f765a8bd5d
Rafaela <rafaelagg22@gmail.com>
2026-05-29 22:08:42 -0300

    executor

--- srcs/execution/executor.c
+++ srcs/execution/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chiaronero <chiaronero@student.42.fr>      +#+  +:+       +#+        */
+/*   By: rafagg <rafagg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 20:32:02 by rgomes-g          #+#    #+#             */
-/*   Updated: 2026/05/27 23:23:43 by chiaronero       ###   ########.fr       */
+/*   Updated: 2026/05/29 22:08:29 by rafagg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ void	exec_child(t_cmd *cmd, t_shell *shell, int fd_in, int *p_fd)
 	setup_pipe_child(fd_in, p_fd, cmd);
 	if (apply_redirs(cmd->redirs, shell) == -1)
 		exit(1);
+	if (!cmd->args || !cmd->args[0])
+		exit(0);
 	if (is_builtin(cmd->args[0]))
 		exit(run_builtin(cmd, shell));
 	exec_external(cmd, shell);
