81819f90c90e32fa25f8ea5ebb08310206c6ccd1
RafaelaGGuimaraes <rafaelagg22@gmail.com>
2026-06-04 13:12:51 -0300

    utils

--- srcs/shared/utils.c
+++ srcs/shared/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rafagg <rafagg@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rgomes-g <rgomes-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 19:46:45 by rgomes-g          #+#    #+#             */
-/*   Updated: 2026/05/29 20:11:21 by rafagg           ###   ########.fr       */
+/*   Updated: 2026/06/04 13:12:30 by rgomes-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,14 @@ void	shell_error(char *cmd, char *msg, int code)
 	}
 	ft_putendl_fd(msg, STDERR_FILENO);
 	(void)code;
+}
+
+void	cleanup_shell(t_shell *shell)
+{
+	free_tokens(shell->tokens);
+	free_cmds(shell->cmds);
+	free_array(shell->env);
+	shell->tokens = NULL;
+	shell->cmds = NULL;
+	shell->env = NULL;
 }
